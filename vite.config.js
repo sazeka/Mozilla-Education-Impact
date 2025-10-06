273b0b02dc3f9cf6517bb3cceb103917c7e1730b
Steven Azeka <36303808+sazeka@users.noreply.github.com>
2025-10-06 13:14:20 -0400

    Updated the vite.config file
    Updated again to publish to github

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// ✅ Replace REPOSITORY_NAME with your actual repo name
+// ✅ Correct base path for GitHub Pages
 export default defineConfig({
   base: '/Mozilla-Education-Impact/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
